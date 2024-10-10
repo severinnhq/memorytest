53dd5654d66cb9aba6ea63868ffedd458475ff92
severinnhq <sstierbusiness@gmail.com>
2024-10-10 17:28:03 +0200

    another mongodb issue solved

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -7,7 +7,7 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI
 const options = {}
 
-let client
+let client: MongoClient
 let clientPromise: Promise<MongoClient>
 
 declare global {
